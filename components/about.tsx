acd9d80bcab48230aa92ae0b7708ce9723432153
Puga <willian.puga@gmail.com>
2024-01-08 20:55:14 -0300

    feat: create about section component and seaction-heading

---
+++ components/about.tsx
@@ -0,0 +1,35 @@
+"use client";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+
+export default function About() {
+  return (
+    <motion.section
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3 ">
+        I am a Software Developer with four years of experience in{" "}
+        <span className="font-medium">Node.js, React, and TypeScript</span>,
+        Passionate about
+        <span className="font-medium"> technology and problem-solving</span>. I
+        am consistently striving to enhance my skills and provide innovative
+        solutions. I bring proven expertise in high-traffic projects, capable of
+        developing{" "}
+        <span className="underline">
+          responsive, optimized, and high-performance applications.
+        </span>{" "}
+        My core stack is{" "}
+        <span className="font-medium">TypeScript, React, and NodeJs</span>
+        .I am also familiar with Ruby on Rails. I am always looking to learn new
+        technologies. My currently position is{" "}
+        <span className="font-medium">Front end </span>
+        developer.
+      </p>
+      <p></p>
+    </motion.section>
+  );
+}
