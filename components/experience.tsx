51a07bfc1f785eeba3538789292ca3c140796464
Puga <willian.puga@gmail.com>
2024-01-15 22:21:52 -0300

    feat: finish skills sections and create experience section

---
+++ components/experience.tsx
@@ -0,0 +1,14 @@
+import SectionHeading from "./section-heading";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+
+export default function Experience() {
+  return (
+    <section id="experience">
+      <SectionHeading>My Experiences</SectionHeading>
+    </section>
+  );
+}
